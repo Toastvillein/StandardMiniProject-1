5a3618a6952ed7a728c11f39a8d33ede2d265265
Toastvillein <okkcba7899@gmail.com>
2025-03-21 15:32:06 +0900

    first commit

---
+++ src/test/java/com/example/springminipractice/SpringMiniPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springminipractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMiniPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
